420e7f8e36f3aca999d5c5b3f07a02bf5fdef9d9
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2024-03-15 11:21:28 +0200

    doing conditionals task

--- intro-to-js-practice.js
+++ intro-to-js-practice.js
@@ -8,14 +8,24 @@ let remainMoney = takenMoney - totalBuyingCost;
 console.log(remainMoney);
 
 // Practice_________Avarage Marks_________2
-let mathematicsMarks =75.23;
+let mathematicsMarks = 75.23;
 const biologyMarks = 65;
 const chemistryMarks = 80;
 const physicsMarks = 35.45;
 const banglaMarks = 99.50;
 
 let totalMarks = mathematicsMarks + biologyMarks + chemistryMarks + physicsMarks + banglaMarks;
-let avarageMarks = totalMarks/5;
+let avarageMarks = totalMarks / 5;
 console.log(parseFloat(avarageMarks.toFixed(2)));
 
 // Practice___________remainder_______3
+let theNumber = 119;
+let remainder = 119 % 5;
+console.log(remainder);
+
+// Practice______isNan________4
+var a = isNaN('11');
+console.log(a);
+var b = isNaN(2-10);
+console.log(b)
+
---
+++ js-conditionals-task.js
@@ -0,0 +1,8 @@
+// Practice task_______Free Drinks
+let burgerBudget = 550;
+if(burgerBudget >= 500){
+    console.log("Get e free coke");
+}
+else{
+    console.log("Buy a Coke.");
+}
