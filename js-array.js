1779ab65fa0f03bffb1449fb1d88288ece490cde
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2024-03-16 12:08:59 +0200

    practicing array

---
+++ js-array.js
@@ -0,0 +1,16 @@
+// Task-1
+let names = ['jack', 'mac', 'joe', 'sargey'];
+console.log(names[3]);
+names[1]= 'jambura';
+console.log(names);
+
+// Task-2
+let placees = ['sajek', 'coxs bazar', 'bandarban', 'ranga mati', 'sondarban'];
+placees.push('kuakata');
+console.log(placees);
+placees.unshift('jaflong', 'tea garden');
+console.log(placees);
+placees.pop();
+console.log(placees);
+
+// Task-3
