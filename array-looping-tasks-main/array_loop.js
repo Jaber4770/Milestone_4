d86b4791258332830b011104ad6c206097256c35
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2024-03-25 02:51:19 +0200

    all the task is complete

--- array-looping-tasks-main/array_loop.js
+++ array-looping-tasks-main/array_loop.js
@@ -15,8 +15,29 @@ console.log(reverseArray);
 const numbers = [12, 98, 5, 41, 23, 78, 46];
 let evenNumber = [];
 for (number of numbers) {
-    if(number % 2 === 0){
+    if (number % 2 === 0) {
         evenNumber.push(number);
     }
 }
 console.log(evenNumber);
+
+
+//  Task-3:
+var words = ['Tom', 'Tim', 'Tin', 'Tik'];
+let string = " ";
+for (word of words) {
+    string = string.concat(word);
+}
+
+console.log(string);
+
+
+//  Task-4:
+const statement = 'I am a hard working person';
+let wordsOfStatement = statement.split(" ");
+let reversedLine = [];
+for (singleWord of wordsOfStatement) {
+    reversedLine.unshift(singleWord);
+}
+let completeReversedLine = reversedLine.join(' ');
+console.log(completeReversedLine);
